c394886744d53d4f86052f53bf9986de4b7dfdaf
Gus G <gusg21@parkschool.net>
2017-01-16 14:01:06 -0500

    Update os-me.js

--- os-me.js
+++ os-me.js
@@ -1,12 +1,27 @@
-var MyLib = {
-    vars: {
-        var1: 'value1',
-        var2: 'value2'
-    },
-    func1: function () {
-        return this.vars.var1;
-    },
-    func2: function () {
-        alert("This is func2");
-    }
+/*
+    os-me: the most likely non-functional os-detection library
+    author: gusg21
+    year: 2017
+    awesome: probably not
+*/
+
+/*	os Object Constructor
+========================*/
+
+function os() {
+		return this;
+}
+
+/*	os Prototype Functions
+============================*/
+
+os.prototype = {
+	clientOS:	function () {
+                var OSName="Unknown OS";
+                if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
+                if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
+                if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
+                if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
+				return OSName;
+			},
 };
